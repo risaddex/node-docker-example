1792d129d92a86aa7fb111b4162c80d5e157a38b
risaddex <risaddex@gmail.com>
2021-05-02 08:56:03 -0300

    first part of class

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:14.15.4-slim
+WORKDIR /app
+COPY package.json .
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+        then npm install; \
+        else npm install --only=production; \
+        fi
+
+COPY . ./
+ENV PORT=3000
+EXPOSE $PORT
+CMD [ "npm", "run", "dev"]
+
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
+const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hi there !aa!aaa! </h2>");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+//! TIME LEFT: 2:49:00
